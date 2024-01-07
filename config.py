14b52a5394aed5e05fe5c8f3ea41107a2f2610f3
عبدالعالم <141892190+apdelelem123@users.noreply.github.com>
2024-01-07 07:58:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 1095638828))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/YY5Y8")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/rr_r_v")
